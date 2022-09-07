d157acfa36a12677163d67338e4ba1c3c919129d
INT\Francisco.Gamez <Francisco.Gamez@Solera.com>
2022-09-07 09:47:53 -0500

    Add HelloWorld java class

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * HelloWorld
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde jenkins ocupando el token -> secrettoken");
+    }
+    
+}
